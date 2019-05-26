9e8d363b72f25a1246da2594758f967f05f0bc05
Ony Arianto <onyariantoprapanca@gmail.com>
2019-05-27 04:27:57 +0700

    fix android error

--- android/app/src/main/java/com/testdot/MainApplication.java
+++ android/app/src/main/java/com/testdot/MainApplication.java
@@ -1,10 +1,11 @@
-package com.pleky.testdot;
+package com.testdot;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+            new RNFirebasePackage()
       );
     }
 
